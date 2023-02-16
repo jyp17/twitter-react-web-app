91d550fd784a62f4ce3d14976cb3d16adf637b0d
jyp17 <jypark1024@gmail.com>
2023-02-16 14:07:22 -0800

    Fix explore component rendering

--- public/tuiter/ExploreScreen/index.js
+++ public/tuiter/ExploreScreen/index.js
@@ -4,18 +4,16 @@ import ExploreComponent from "./ExploreComponent.js";
 
 function exploreComp() {
     $('#wd-explore').append(`
-         <div class="row mt-2">
-         <div class="row mt-2">
-             <div class="col-2 col-md-2 col-lg-1 col-xl-2">
-                ${NavigationSidebar(`explore`)}
-             </div>
-             <div class="col-10 col-lg-7 col-xl-6">
-                ${ExploreComponent()}
-             </div>
-             <div class="d-none d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
-                ${WhoToFollowList()}
-             </div>
-        </div>
+       <div class="row mt-2">
+         <div class="col-2 col-md-2 col-lg-1 col-xl-2">
+            ${NavigationSidebar(`explore`)}
+         </div>
+         <div class="col-10 col-lg-7 col-xl-6">
+            ${ExploreComponent()}
+         </div>
+         <div class="d-none d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
+            ${WhoToFollowList()}
+         </div>
        </div>
    `);
 }

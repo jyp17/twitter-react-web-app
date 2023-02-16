53bc9cafb9e4d36249f80c42646bb1f65c67dd66
jyp17 <jypark1024@gmail.com>
2023-02-16 14:13:45 -0800

    Spacing

--- public/tuiter/HomeScreen/index.js
+++ public/tuiter/HomeScreen/index.js
@@ -4,19 +4,17 @@ import PostList from "../PostList/index.js"
 
 function homeComponent() {
     $('#wd-home').append(`
-         <div class="row mt-2">
-         <div class="row mt-2">
-         <div class="col-2 col-md-2 col-lg-1 col-xl-2">
+        <div class="row mt-2">
+            <div class="col-2 col-md-2 col-lg-1 col-xl-2">
             ${NavigationSidebar(`home`)}
-         </div>
-         <div class="col-10 col-lg-7 col-xl-6">
+            </div>
+            <div class="col-10 col-lg-7 col-xl-6">
             ${PostList()}
-         </div>
-         <div class="d-none d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
+            </div>
+            <div class="d-none d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
             ${PostSummaryList()}
-         </div>
-       </div>
-       </div>
+            </div>
+        </div>
    `);
 }
 

a2a2d1a5e353e87196dce8c0fb78274d747ffa13
jyp17 <jypark1024@gmail.com>
2023-02-16 12:25:02 -0800

    Fix tags

--- public/tuiter/ExploreScreen/index.js
+++ public/tuiter/ExploreScreen/index.js
@@ -1,6 +1,7 @@
 import NavigationSidebar from "../NavigationSidebar/index.js";
 import WhoToFollowList from "../WhoToFollowList/index.js";
 import ExploreComponent from "./ExploreComponent.js";
+
 function exploreComp() {
     $('#wd-explore').append(`
          <div class="row mt-2">
@@ -14,6 +15,7 @@ function exploreComp() {
          <div class="d-none d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
             ${WhoToFollowList()}
          </div>
+       </div>
        </div>
    `);
 }
--- public/tuiter/HomeScreen/index.js
+++ public/tuiter/HomeScreen/index.js
@@ -16,6 +16,7 @@ function homeComponent() {
             ${PostSummaryList()}
          </div>
        </div>
+       </div>
    `);
 }
 

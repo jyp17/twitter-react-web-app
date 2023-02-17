6ec2638b7aecbaf13ce315ebdebb3c7a0aa9df4b
jyp17 <jypark1024@gmail.com>
2023-02-16 17:14:30 -0800

    Lab portion

---
+++ src/labs/index.js
@@ -0,0 +1,12 @@
+import Assignment6 from "./a6/index.js";
+import Nav from "../nav";
+function Labs() {
+    return(
+        <div>
+            <Nav/>
+            <Assignment6 />
+        </div>
+    );
+}
+export default Labs;
+

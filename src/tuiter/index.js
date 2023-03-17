2d2b0f86ae9df44bb92eff885c78e9f9edbc5e70
jyp17 <jypark1024@gmail.com>
2023-03-17 15:22:19 -0700

    Default to explore page

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -9,11 +9,12 @@ function Tuiter() {
     return (
         <div className="row mt-2">
             <div className="col-2 col-md-2 col-lg-1 col-xl-2">
-                <NavigationSidebar active="explore"/>
+                <NavigationSidebar />
             </div>
             <div className="col-10 col-md-10 col-lg-7 col-xl-6"
                  style={{"position": "relative"}}>
                 <Routes>
+                    <Route index element={<ExploreComponent/>}/>
                     <Route path="home" element={<HomeComponent/>}/>
                     <Route path="explore" element={<ExploreComponent/>}/>
                 </Routes>
--- src/tuiter/navigation-sidebar/index.js
+++ src/tuiter/navigation-sidebar/index.js
@@ -5,7 +5,8 @@ import {useLocation} from "react-router";
 const NavigationSidebar = () => {
     const {pathname} = useLocation();
     const paths = pathname.split('/');
-    const active = paths[2];
+    console.log(paths);
+    const active = paths.length === 2 || paths[2] === ''? 'explore' : paths[2];
 
     return (
         <div className="list-group">

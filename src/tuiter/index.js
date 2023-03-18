f8919e346cf4cc65501216ea9d9c296f097c77b8
jyp17 <jypark1024@gmail.com>
2023-03-17 17:40:27 -0700

    Implement post summary reducer

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -5,12 +5,14 @@ import WhoToFollowList from "./who-to-follow-list";
 import HomeComponent from "./home";
 import {Routes, Route} from "react-router";
 import whoReducer from "./reducers/who-reducer";
+import tuitsReducer from "./tuits/tuits-reducer";
 import { configureStore } from '@reduxjs/toolkit';
 import {Provider} from "react-redux";
 
 const store = configureStore({
     reducer: {
-        who: whoReducer
+        who: whoReducer,
+        tuits: tuitsReducer
     }
 });
 
